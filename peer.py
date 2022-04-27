0372e16f87dc4f9b681d9ad6aa41e6683ab2a818
Tasneem safia <76720686+Tasneem-Abu-Safia@users.noreply.github.com>
2022-04-27 19:59:08 +0300

    P2P

--- peer.py
+++ peer.py
@@ -1,85 +1,69 @@
-import socket 
-import threading
+<?php
+/*
+* server.php
+*/
 
-HEADER = 64
-PORT = 5050
-NODE = socket.gethostbyname(socket.gethostname())
-ADDR = (NODE, PORT)
 
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!exit"
+/*
+* Init
+*/
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+//127.0.0.1
+//1337
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
 
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
 
-            print(f"[{addr}] {msg}")
-            #input()
-            if(msg == 'version'):
-                print("Send version")
-                conn.send("verack".encode(FORMAT))
-                print("Send version acknowledgement")
-                conn.send("version".encode(FORMAT))
-            elif (msg == 'getaddr'):
-                print("Send address")
-                conn.send(NODE.encode(FORMAT))
-            else:
-                print("Send address received")
-                conn.send(NODE.encode(FORMAT))
-                
 
-    conn.close()
+$my_ip_address = getHostByName(getHostName());
 
-def start():
-    node.listen()
-    print(f"[LISTENING] Node is listening on {NODE}")
-    while True:
-        conn, addr = node.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}")
+$alt_ip_address = "192.168.75.186";
 
-try:
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    print("Send version")
-    send("version")
-    #input()
-    print("Send version acknowledgement")
-    send("verack")
-    #input()
-    print("Send address")
-    send("addr")
-    #input()
-    print("Send address received")
-    send("getaddr")
-    #input()
-    peerconnected = True
-    while peerconnected:
-        msg = input("Msg>>")
-        send(msg)
-        if msg == DISCONNECT_MESSAGE:
-            peerconnected = False
-except:
-    print("No peers")
-    node = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    node.bind(ADDR)
-    print("[STARTING] Node starting...")
-    start()
+$port = 1337;
+// Socket resource
+$server = stream_socket_server("tcp://$alt_ip_address:$port", $errno, $errorMessage);
+
+
+echo "\n>>> Reporting for duty\n\n";
+
+//echo "> My IP: " . $my_ip_address . PHP_EOL;
+echo "> My Alt IP: " . $alt_ip_address . PHP_EOL;
+
+/*
+* Code
+*/
+if ($server === false) {
+    throw new UnexpectedValueException("Could not bind to socket: $errorMessage");
+} else {
+
+  echo "> Server is OK: " . $server . PHP_EOL;
+
+}
+
+for (;;) {
+
+    echo "> Listening for Client..." . PHP_EOL;
+
+    $client = @stream_socket_accept($server);
+
+    if ($client) {
+        
+	echo "> Message Received from Client" . PHP_EOL;
+	$sock_data = fread($client, 1024);
+	echo $sock_date . PHP_EOL;
+	echo "> Message Complete" . PHP_EOL;
+	//echo fwrite($client, "Hello! The time is ".date("n/j/Y g:i a")."\n");
+        //echo stream_copy_to_stream($client, $client) . PHP_EOL;
+	//$pkt = stream_socket_recvfrom($client, 1, 0, $peer);
+	//echo $pkt . PHP_EOL;
+
+        fclose($client);
+    }
+}
+
+
+
+/*
+* Finally
+*/
+
+echo "\n>>> End of Program!\n\n";
